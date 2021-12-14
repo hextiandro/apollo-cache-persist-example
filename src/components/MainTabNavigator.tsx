2e5da37067707540827310045dbe531913814379
asio <alonsoasiel@gmail.com>
2021-12-14 16:14:23 -0500

    refactor code

--- src/components/MainTabNavigator.tsx
+++ src/components/MainTabNavigator.tsx
@@ -1,18 +1,12 @@
-// import { Feather } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigatorScreenParams } from "@react-navigation/native";
 import { BottomTabBar } from "@react-navigation/bottom-tabs";
 import React from "react";
-// import {
-//     CharactersStackNavigator,
-//     CharactersStackParamList,
-// } from "./CharactersStackNavigator";
-// import { HomeScreen, MoreScreen } from "@app/screens";
-// import { OfflineBanner } from "@app/components";
 import { CharactersStackNavigator, CharactersStackParamList } from "./CharacterStackNavigator";
 import { HomeScreen } from "../screens/HomeScreen";
 import { MoreScreen } from "../screens/MoreScreen";
 import { OfflineBanner } from "./OfflineBanner";
+import { View } from "react-native";
 
 export type MainTabParamList = {
     Home: undefined;
@@ -34,35 +28,23 @@ export const MainTabNavigator: React.FC = () => {
             <MainTab.Screen
                 name="Home"
                 component={HomeScreen}
-                // options={{
-                //     tabBarIcon: ({ color, size }) => {
-                //         return (
-                //             <Feather name="home" size={size} color={color} />
-                //         );
-                //     },
-                // }}
+                options={{
+                    tabBarIcon: () => <View />
+                }}
             />
             <MainTab.Screen
                 name="Characters"
                 component={CharactersStackNavigator}
-                // options={{
-                //     tabBarIcon: ({ color, size }) => {
-                //         return (
-                //             <Feather name="users" size={size} color={color} />
-                //         );
-                //     },
-                // }}
+                options={{
+                    tabBarIcon: () => <View />
+                }}
             />
             <MainTab.Screen
                 name="More"
                 component={MoreScreen}
-                // options={{
-                //     tabBarIcon: ({ color, size }) => {
-                //         return (
-                //             <Feather name="menu" size={size} color={color} />
-                //         );
-                //     },
-                // }}
+                options={{
+                    tabBarIcon: () => <View />
+                }}
             />
         </MainTab.Navigator>
     );
